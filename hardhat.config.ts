98ce9e98e9776c3862619aff2adec655c72b7acc
Coderdan <dmathieuva@gmail.com>
2024-09-27 20:25:16 +0800

    chore: convert to TS

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,16 +1,9 @@
-
 /* global ethers task */
-require('@nomiclabs/hardhat-waffle')
+import "@nomiclabs/hardhat-ethers";
+// import "@nomicfoundation/hardhat-chai-matchers";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task('accounts', 'Prints the list of accounts', async () => {
-  const accounts = await ethers.getSigners()
-
-  for (const account of accounts) {
-    console.log(account.address)
-  }
-})
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -19,11 +12,11 @@ task('accounts', 'Prints the list of accounts', async () => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: '0.8.6',
+  solidity: "0.8.6",
   settings: {
     optimizer: {
       enabled: true,
-      runs: 200
-    }
-  }
-}
+      runs: 200,
+    },
+  },
+};
